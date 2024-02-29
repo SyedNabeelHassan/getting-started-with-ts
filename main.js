c671e5a6032748653b60e2aa4ee1b35d03fdd101
SyedNabeelHassan <160429260+SyedNabeelHassan@users.noreply.github.com>
2024-02-29 16:33:21 +0500

    first

---
+++ main.js
@@ -0,0 +1,10 @@
+// updating value of var
+var myName;
+myName = 'stock01';
+myName = 'stock02';
+// const can't be updated
+console.log(myName);
+var f = '123';
+console.log(f);
+//  reserved values workaround name is reserved so we use $name but not all like #,%
+// variables containers for data storage
---
+++ main.ts
@@ -0,0 +1,10 @@
+// updating value of var
+let myName;
+myName = 'stock01';
+myName = 'stock02';
+// const can't be updated
+ console.log(myName);
+ const
+ f = '123';
+ console.log(f)
+//  reserved values workaround name is reserved so we use $name but not all like #,%
